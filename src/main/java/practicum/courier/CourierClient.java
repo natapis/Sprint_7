2a12cf5b786fc6d1539bf65759a0425190af932b
natapis <natapis2009@mail.ru>
2023-08-20 15:07:42 +0300

    Добавила параметр в ручку

--- src/main/java/practicum/courier/CourierClient.java
+++ src/main/java/practicum/courier/CourierClient.java
@@ -7,36 +7,32 @@ import static io.restassured.RestAssured.given;
 public class CourierClient {
     private static final String URL_CREATE = "/api/v1/courier";
     private static final String URL_LOGIN = "/api/v1/courier/login";
-    private static final String URL_DELETE = "/api/v1/courier/:id";
+    private static final String URL_DELETE = "/api/v1/courier/{:id}";
 
-    public CourierClient(){
+    public CourierClient() {
 
     }
 
-    public Response createCourier(Courier courier){
+    public Response createCourier(Courier courier) {
         return given()
-                .header("Content-type","application/json")
+                .header("Content-type", "application/json")
                 .and()
                 .body(courier)
                 .when()
                 .post(URL_CREATE);
     }
 
-    public Response loginCourier(CourierCreds courierCreds){
+    public Response loginCourier(CourierCreds courierCreds) {
         return given()
-                .header("Content-type","application/json")
+                .header("Content-type", "application/json")
                 .and()
                 .body(courierCreds)
                 .when()
                 .post(URL_LOGIN);
     }
 
-    public Response deleteCourier(String id){
+    public Response deleteCourier(String id) {
         return given()
-                .header("Content-type","application/json")
-                .body(id)
-                .queryParam("id", id)
-                .when()
-                .delete(URL_DELETE);
+                .delete(URL_DELETE, id);
     }
 }
